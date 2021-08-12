71a645eaaf6f8518f0bc4553ad96c8c42813c8f1
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-08-11 21:08:49 -0400

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,47 @@ import {
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ENR, Discv5 } from '@chainsafe/discv5'
+import PeerId from 'peer-id'
+import { Multiaddr } from "multiaddr"
 
-export const App = () => (
+export const App = () => {
+  const [discv5, setDiscv5] = React.useState<Discv5>()
+  const [enr, setENR] = React.useState<string>()
+  const init = async () => {
+    const id = await PeerId.create({ keyType: 'secp256k1' })
+    const enr = ENR.createFromPeerId(id)
+    //@ts-ignore
+    enr.setLocationMultiaddr(new Multiaddr('/ip4/127.0.0.1/tcp/4001/wss'))
+    //@ts-ignore
+    const discv5 = Discv5.create({enr: enr, peerId: id, multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4001/wss'), transport: 'wss'})
+    //@ts-ignore
+    window.discv5 = discv5;
+    //@ts-ignore
+    window.Multiaddr = Multiaddr
+    setDiscv5(discv5);
+    await discv5.start();
+    console.log('started discv5', discv5.isStarted())
+    setENR(discv5.enr.encodeTxt(discv5.keypair.privateKey))
+    discv5.on('discovered', (msg) => console.log(msg))
+    discv5.on('talkReqReceived', (msg) => console.log(msg))
+  }
+
+
+  React.useEffect(() => {
+    init();
+  },[])
+
+  return (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Text>Is Discv5 here?</Text>
+          {enr && <Text>{enr}</Text>}
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+)}

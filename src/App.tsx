5a2523084f42b88feb53c7f86923c07e313db560
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-09-01 12:01:46 -0400

    Add console logs for talkreq/resp events

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,70 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { ENR, Discv5 } from '@chainsafe/discv5'
-import PeerId from 'peer-id'
-import { Multiaddr } from "multiaddr"
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { ENR, Discv5 } from "@chainsafe/discv5";
+import PeerId from "peer-id";
+import { Multiaddr } from "multiaddr";
 
 export const App = () => {
-  const [discv5, setDiscv5] = React.useState<Discv5>()
-  const [enr, setENR] = React.useState<string>()
+  const [discv5, setDiscv5] = React.useState<Discv5>();
+  const [enr, setENR] = React.useState<string>();
+
+  React.useEffect(() => {
+    if (discv5) {
+      setENR(discv5.enr.encodeTxt(discv5.keypair.privateKey))
+    }
+  },[discv5])
   const init = async () => {
-    const id = await PeerId.create({ keyType: 'secp256k1' })
-    const enr = ENR.createFromPeerId(id)
+    const id = await PeerId.create({ keyType: "secp256k1" });
+    const enr = ENR.createFromPeerId(id);
     //@ts-ignore
-    enr.setLocationMultiaddr(new Multiaddr('/ip4/127.0.0.1/tcp/4001/wss'))
+    enr.setLocationMultiaddr(new Multiaddr("/ip4/127.0.0.1/tcp/0"));
     //@ts-ignore
-    const discv5 = Discv5.create({enr: enr, peerId: id, multiaddr: new Multiaddr('/ip4/127.0.0.1/tcp/4001/wss'), transport: 'wss'})
+    const discv5 = Discv5.create({
+      enr: enr,
+      peerId: id,
+      //@ts-ignore
+      multiaddr: new Multiaddr("/ip4/127.0.0.1/tcp/0"),
+      transport: "wss",
+    });
     //@ts-ignore
     window.discv5 = discv5;
     //@ts-ignore
-    window.Multiaddr = Multiaddr
+    window.Multiaddr = Multiaddr;
+    //@ts-ignore
+    window.ENR = ENR;
     setDiscv5(discv5);
     await discv5.start();
     discv5.enableLogs();
-    console.log('started discv5', discv5.isStarted())
-    setENR(discv5.enr.encodeTxt(discv5.keypair.privateKey))
-    discv5.on('discovered', (msg) => console.log(msg))
-    discv5.on('talkReqReceived', (msg) => console.log(msg))
-  }
-
+    console.log("started discv5", discv5.isStarted());
+    setENR(discv5.enr.encodeTxt(discv5.keypair.privateKey));
+    discv5.on("discovered", (msg) => console.log(msg));
+    discv5.on("talkReqReceived", (srcId, enr, msg) => console.log('content requested',msg.request.toString()));
+    discv5.on("talkRespReceived", (srcId, enr, msg) => console.log('what we got back', msg.response.toString()))
+  };
 
   React.useEffect(() => {
     init();
-  },[])
+  }, []);
 
   return (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Text>Is Discv5 here?</Text>
-          {enr && <Text>{enr}</Text>}
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)}
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Text>Is Discv5 here?</Text>
+            {enr && <Text>{enr}</Text>}
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};

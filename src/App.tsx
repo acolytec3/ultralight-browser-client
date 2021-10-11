9d1529d9af59ef2b39fef37de55c7464ed2afc8b
ScottyPoi <kyzambo@gmail.com>
2021-10-11 01:42:13 -0600

    Add ShowInfo component
    displays information about node and ENR

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { ENR, Discv5 } from "@chainsafe/discv5";
 import PeerId from "peer-id";
 import { Multiaddr } from "multiaddr";
+import ShowInfo from "./Components/ShowInfo";
 
 export const App = () => {
   const [discv5, setDiscv5] = React.useState<Discv5>();
@@ -59,8 +60,9 @@ export const App = () => {
 
   return (
     <ChakraProvider theme={theme}>
+      {discv5 && <ShowInfo discv5={discv5} />}
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+        <Grid minH="50vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Text>Is Discv5 here?</Text>

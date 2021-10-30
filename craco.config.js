b71357aec82d7e2481ede8eaf83ff55472584e9e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-10-29 22:37:35 -0400

    Handle more React weirdness

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,8 @@ module.exports = {
     babel: {
         plugins: [
             "@babel/plugin-proposal-nullish-coalescing-operator",
-            "@babel/plugin-proposal-class-properties"
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-optional-chaining"
         ],
     }
 }
